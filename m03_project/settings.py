556a668296e94351b4e8a0fd0beb44f560268fa2
Willaim Batista <bootlegbilly@protonmail.ch>
2022-10-04 16:49:19 -0400

    Added an index.html page

--- m03_project/settings.py
+++ m03_project/settings.py
@@ -33,6 +33,7 @@ ALLOWED_HOSTS = []
 INSTALLED_APPS = [
     'authentication.apps.AuthenticationConfig',
     'items.apps.ItemsConfig',
+    "base_site.apps.BaseSiteConfig",
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
